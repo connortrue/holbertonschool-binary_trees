e0ada3e8cec72a81cfe86d357724e007f7bd00f0
connortrue <connor.true.94@gmail.com>
2023-08-16 18:59:52 -0500

    hack

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,15 +6,15 @@
  *
  * Return: height of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height_helper(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height_helper(tree->left);
+	right_height = binary_tree_height_helper(tree->right);
 
 	if (left_height > right_height)
 		return (left_height + 1);
@@ -22,3 +22,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (right_height + 1);
 }
 
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of the tree minus 1, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height_helper(tree) - 1);
+}
